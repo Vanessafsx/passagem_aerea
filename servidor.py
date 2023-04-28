24c1a0269be435ce73eeab62cd853cc59417ecc8
AdsonBruno <adsonbruno2@gmail.com>
2023-04-28 11:40:48 -0300

    fix: correcao de nome da cateogria, de economy para economica e add mensagem de servidor rodando

--- servidor.py
+++ servidor.py
@@ -8,14 +8,15 @@ class ServicoDeVendas(vendas_aereas_pb2_grpc.VendasServicer):
         # Aqui você pode inserir a lógica para verificar a disponibilidade de voos com base nos dados fornecidos pelo cliente.
         # No exemplo abaixo, estamos apenas retornando uma resposta fixa.
         disponivel = True
-        categoria = vendas_aereas_pb2.ECONOMY
+        categoria = vendas_aereas_pb2.ECONOMICA
         resposta = vendas_aereas_pb2.RespostaDisponibilidade(disponivel=disponivel, categoria=categoria)
         return resposta
 
 def servir():
     servidor = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     vendas_aereas_pb2_grpc.add_VendasServicer_to_server(ServicoDeVendas(), servidor)
-    servidor.add_insecure_port('[::]:50051')
+    porta = servidor.add_insecure_port('[::]:50051')
+    print(f'Servidor gRPC rodando na porta {porta}')
     servidor.start()
     servidor.wait_for_termination()
 
