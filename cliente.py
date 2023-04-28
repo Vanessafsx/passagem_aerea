03874925174eb03fd0ff062462bcb9df53f70908
AdsonBruno <adsonbruno2@gmail.com>
2023-04-28 11:38:49 -0300

    fix: correcao do metodo de RespostaDisponibilidade para SolicitacaoDisponibilidade

--- cliente.py
+++ cliente.py
@@ -6,17 +6,24 @@ def executar():
     with grpc.insecure_channel('localhost:50051') as canal:
         stub = vendas_aereas_pb2_grpc.VendasStub(canal)
         resposta = stub.VerificarDisponibilidade(
-            vendas_aereas_pb2.RespostaDisponibilidade(
-                data_partida='2023-05-01',
-                data_retorno='2023-05-10',
-                origem='SFO',
-                destino='LAX'
+            vendas_aereas_pb2.RequisicaoDisponibilidade(
+                data_partida ='2023-05-01',
+                data_retorno = '2023-05-10',
+                origem = 'SFO',
+                destino = 'LAX'
             )
         )
+
         if resposta.disponivel:
-            print(f"Assentos disponíveis na categoria {resposta.categoria}.")
+            # print(f'Assentos disponíveis na categoria {resposta.categoria}.')
+            if resposta.categoria == vendas_aereas_pb2.Categoria.ECONOMICA:
+                print("Assentos disponíveis na categoria econômica.")
+            elif resposta.categoria == vendas_aereas_pb2.Categoria.EXECUTIVA:
+                print("Assentos disponíveis na categoria executiva.")
+            elif resposta.categoria == vendas_aereas_pb2.Categoria.PRIMEIRA_CLASSE:
+                print("Assentos disponíveis na categoria primeira classe.")
         else:
-            print("Não há assentos disponíveis para o itinerário solicitado.")
+            print('Não há assentos disponíveis para o itinerário solicitado.')
 
 if __name__ == '__main__':
     executar()
